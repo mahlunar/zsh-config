01176af5b1e410e52fbbcb32c018fccf87d1e360
Martin Anker Have <martin.have@gmail.com>
2021-03-12 10:35:24 +0100

    Initial setup

---
+++ setup.zsh
@@ -0,0 +1,16 @@
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+export PATH="${PATH}:${HOME}/.krew/bin"
+
+export SRC_ENDPOINT=https://sourcegraph.lunar.tech
+
+export LOG_CONSOLE_AS_JSON=false
+
+LW_PATH=lunar
+
+alias s="shuttle"
+alias sr="shuttle run --skip-pull"
+alias h="hamctl"
+
+source ~/.zsh-config/go.zsh
+source ~/.zsh-config/secret.zsh
