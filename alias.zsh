f9afa4c08b819e850824159b9c1dcfe4a734503a
Martin Anker Have <martin.have@gmail.com>
2022-06-03 08:47:26 +0200

    Update aliases with terraform and go fmt

--- alias.zsh
+++ alias.zsh
@@ -2,6 +2,10 @@ alias s="shuttle"
 alias sr="shuttle run --skip-pull"
 
 alias h="hamctl"
+alias t="terraform"
+
 alias gocode="cd /Users/martinankerhave/lunar/Github"
 alias bs="eval 'open https://backstage.lunar.tech/'"
 alias ghw="eval 'open https://www.github.com/'"
+
+alias gfmt="gofmt -l -w ."
